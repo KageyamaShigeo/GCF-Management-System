10f2361e54376b5c42d9d4ea7e673fb238b95f68
Daniel Peralta <40254634+Danny101Techology@users.noreply.github.com>
2022-07-26 19:47:12 +0800

    Update Default.vue

--- gcf-ms-front/src/layouts/Default.vue
+++ gcf-ms-front/src/layouts/Default.vue
@@ -12,63 +12,140 @@
 
     <v-main>
       <v-container>
-        <v-row>
-          <v-col v-for="n in 24" :key="n" cols="4">
-            <v-card :loading="loading" class="mx-auto my-12" max-width="374">
-              <template slot="progress">
-                <v-progress-linear
-                  color="deep-purple"
-                  height="10"
-                  indeterminate
-                ></v-progress-linear>
-              </template>
-
-              <v-img
-                height="250"
-                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-              ></v-img>
-
-              <v-card-title>room_name_placeholder</v-card-title>
-
-              <v-card-text>
-
-                <div class="my-4 text-subtitle-1">
-                  type_placeholder
-                </div>
-
-                <div>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sit sapiente repellendus maxime in, vitae vel aspernatur quis odio repellat veniam libero. Id at neque ab, eveniet vel quod recusandae!
-                </div>
-              </v-card-text>
-
-              <v-divider class="mx-4"></v-divider>
-
-              <v-card-actions>
-                <v-btn color="deep-purple lighten-2" text @click="reserve">
-                  Reserve
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
+        <template>
+          <v-data-table
+            :headers="dessertHeaders"
+            :items="desserts"
+            :single-expand="singleExpand"
+            :expanded.sync="expanded"
+            item-key="name"
+            show-expand
+            class="elevation-1"
+          >
+            <template v-slot:top>
+              <v-toolbar flat>
+                <v-toolbar-title>Expandable Table</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-switch
+                  v-model="singleExpand"
+                  label="Single expand"
+                  class="mt-2"
+                ></v-switch>
+              </v-toolbar>
+            </template>
+            <template v-slot:expanded-item="{ headers, item }">
+              <td :colspan="headers.length">More info about {{ item.name }}</td>
+            </template>
+          </v-data-table>
+        </template>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-export default {
-    data: () => ({
-      loading: false,
-      selection: 1,
-    }),
-
-    methods: {
-      reserve () {
-        this.loading = true
-
-        setTimeout(() => (this.loading = false), 2000)
-      },
+  export default {
+    data () {
+      return {
+        expanded: [],
+        singleExpand: false,
+        dessertHeaders: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+          { text: '', value: 'data-table-expand' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+          },
+        ],
+      }
     },
   }
 </script>
