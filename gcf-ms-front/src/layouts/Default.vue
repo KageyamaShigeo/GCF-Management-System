79425ace83362452a16de6364ca67271d3601f4b
Daniel Peralta <40254634+Danny101Techology@users.noreply.github.com>
2022-07-26 18:14:15 +0800

    Update Default.vue

--- gcf-ms-front/src/layouts/Default.vue
+++ gcf-ms-front/src/layouts/Default.vue
@@ -1,9 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      app
-      shrink-on-scroll
-    >
+    <v-app-bar app shrink-on-scroll>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
       <v-spacer></v-spacer>
@@ -16,12 +13,42 @@
     <v-main>
       <v-container>
         <v-row>
-          <v-col
-            v-for="n in 24"
-            :key="n"
-            cols="4"
-          >
-            <v-card height="200"></v-card>
+          <v-col v-for="n in 24" :key="n" cols="4">
+            <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+              <template slot="progress">
+                <v-progress-linear
+                  color="deep-purple"
+                  height="10"
+                  indeterminate
+                ></v-progress-linear>
+              </template>
+
+              <v-img
+                height="250"
+                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+              ></v-img>
+
+              <v-card-title>room_name_placeholder</v-card-title>
+
+              <v-card-text>
+
+                <div class="my-4 text-subtitle-1">
+                  type_placeholder
+                </div>
+
+                <div>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sit sapiente repellendus maxime in, vitae vel aspernatur quis odio repellat veniam libero. Id at neque ab, eveniet vel quod recusandae!
+                </div>
+              </v-card-text>
+
+              <v-divider class="mx-4"></v-divider>
+
+              <v-card-actions>
+                <v-btn color="deep-purple lighten-2" text @click="reserve">
+                  Reserve
+                </v-btn>
+              </v-card-actions>
+            </v-card>
           </v-col>
         </v-row>
       </v-container>
@@ -30,8 +57,19 @@
 </template>
 
 <script>
-  export default {
-    //
+export default {
+    data: () => ({
+      loading: false,
+      selection: 1,
+    }),
+
+    methods: {
+      reserve () {
+        this.loading = true
+
+        setTimeout(() => (this.loading = false), 2000)
+      },
+    },
   }
 </script>
 
@@ -41,5 +79,5 @@ query {
     siteName
   }
 }
-</static-query>
 
+</static-query>
